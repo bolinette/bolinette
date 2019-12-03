01f45f855493aca94d3399d18b3cc659c5b3d6ea
Pierre Chat <pierrechat89@hotmail.fr>
2019-12-03 21:57:49 +0100

    Tests on CI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'bolinette.services',
         'bolinette.testing'
     ],
-    version='0.0.9',
+    version='0.0.10',
     license='MIT',
     description='Bolinette, a web framework built on top of Flask',
     author='Pierre Chat',
