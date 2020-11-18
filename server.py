61d6686f60a61b325ba1ac95918790d46091ba42
Pierre Chat <pierrechat89@hotmail.fr>
2020-11-18 23:43:52 +0100

    Fixed test env

--- server.py
+++ server.py
@@ -1,13 +1,7 @@
-import os
 import sys
-
 # noinspection PyUnresolvedReferences
 import tests
 
 if __name__ == '__main__':
-    command = sys.argv[1]
-    if command == 'run_tests':
-        if 'BLNT_PROFILE' not in os.environ:
-            os.environ['BLNT_PROFILE'] = 'test'
     from example import bolinette
     bolinette.run_command(*sys.argv[1:])
